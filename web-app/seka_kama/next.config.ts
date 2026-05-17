b71d0c6b916e93b3faa0120efc16f0fbae3b6859
Jase Mwaura <rawscript170@gmail.com>
2026-05-17 19:35:31 +0300

    fix config --next

--- web-app/seka_kama/next.config.ts
+++ web-app/seka_kama/next.config.ts
@@ -11,8 +11,43 @@ const nextConfig: NextConfig = {
     '@kepler.gl/processors',
     '@kepler.gl/utils',
     'react-palm',
-    'styled-components'
+    'styled-components',
+    'deck.gl',
+    '@deck.gl/core',
+    '@deck.gl/layers',
+    '@deck.gl/react',
+    '@deck.gl/geo-layers',
+    '@deck.gl/mesh-layers',
+    '@deck.gl/extensions',
+    '@deck.gl/aggregation-layers',
+    '@deck.gl/json',
+    'luma.gl',
+    '@luma.gl/constants',
+    '@luma.gl/core',
+    '@luma.gl/engine',
+    '@luma.gl/shadertools',
+    '@luma.gl/webgl',
+    '@luma.gl/gltools',
+    '@luma.gl/effects',
+    '@luma.gl/gltf',
+    '@math.gl/core',
+    '@math.gl/proj4',
+    '@math.gl/web-mercator'
   ],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            fs: false,
+            path: false,
+            crypto: false,
+            canvas: false,
+            child_process: false,
+            os: false
+        };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
